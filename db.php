ca39a29126f80dc4eef085af27db319a1b0cbffb
zxc <mukhin010309@gmail.com>
2026-05-16 09:49:32 +0600

    сайт хостинг

--- db.php
+++ db.php
@@ -1,3 +1,15 @@
 <?php
-$connect = new mysqli("localhost", "root", "12345678", "rabota");
+$host = "127.0.0.1";
+$port = 3308;
+$user = "mukhin0102";
+$password = "CPN7UQ^TWTUXSaAD";
+$database = "mukhin0102";
+
+$connect = new mysqli($host, $user, $password, $database, $port);
+
+if ($connect->connect_error) {
+    die("Ошибка подключения: " . $connect->connect_error);
+}
+
+$connect->set_charset("utf8mb4");
 ?>
